5043303dc13cd928dc6f73f6ea676d9a914e8ffe
Anthony Elder <torntrousers@gmail.com>
2018-08-19 11:46:27 +0100

    Add simplest fastest esp-now sensor example

---
+++ espnow-sensor-minimal/espnow-sensor-minimal.ino
@@ -0,0 +1,58 @@
+/*
+ ESP-NOW based sensor 
+
+ Sends readings to an ESP-Now server with a fixed mac address
+
+ Anthony Elder
+ License: Apache License v2
+*/
+extern "C" {
+  #include <espnow.h>
+}
+
+// this is the MAC Address of the remote ESP server which receives these sensor readings
+uint8_t remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
+
+#define WIFI_CHANNEL 1
+//#define SLEEP_SECS 15 * 60 // 15 minutes
+#define SLEEP_SECS 5  // 15 minutes
+#define SEND_TIMEOUT 100  // 245 millis seconds timeout 
+
+// keep in sync with slave struct
+struct __attribute__((packed)) SENSOR_DATA {
+  float temp;
+} sensorData;
+
+void setup() {
+  Serial.begin(115200); Serial.println();
+
+  if (esp_now_init() != 0) {
+    Serial.println("*** ESP_Now init failed");
+    gotoSleep();
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
+
+  esp_now_register_send_cb([](uint8_t* mac, uint8_t sendStatus) {
+    Serial.printf("send_cb, send done, status = %i\n", sendStatus);
+    gotoSleep();
+  });
+
+  uint8_t bs[sizeof(sensorData)];
+  memcpy(bs, &sensorData, sizeof(sensorData));
+  esp_now_send(NULL, bs, sizeof(sensorData)); // NULL means send to all peers
+}
+
+void loop() {
+  if (millis() > SEND_TIMEOUT) {
+    gotoSleep();
+  }
+}
+
+void gotoSleep() {
+  // add some randomness to avoid collisions with multiple devices
+  int sleepSecs = SLEEP_SECS + ((uint8_t)RANDOM_REG32/8); 
+  Serial.printf("Awake for %i ms, going to sleep for %i secs...\n", millis(), sleepSecs); 
+  ESP.deepSleepInstant(sleepSecs * 1000000, RF_NO_CAL);
+}
